a23091effdf8949ed5a9a3c675a7aadf801def95
Aditya <adityafav@gmail.com>
2022-11-22 21:43:23 +0530

    refactor: major changes

--- geektrust.js
+++ geektrust.js
@@ -1,9 +1,8 @@
 const fs = require("fs"),
   filename = process.argv[2],
   dayjs = require("dayjs"),
-  customParseFormat = require("dayjs/plugin/customParseFormat");
-// runner = require("./helpers").runner,
-// helpers = require("./helpers");
+  customParseFormat = require("dayjs/plugin/customParseFormat"),
+  { runner } = require("./helpers");
 
 dayjs.extend(customParseFormat);
 
@@ -13,13 +12,13 @@ fs.readFile(filename, "utf8", (err, data) => {
   // console.log(inputLines)
 
   let startDate,
-    topupToAdd = {},
+    subscriptions = [],
+    topups = [],
     amount = 0;
 
   // Add your code here to process input commands
-  // this loop populates the declared varibales above //
+
   for (const line of inputLines) {
-    // console.log(line)
     if (line.startsWith("START_SUBSCRIPTION")) {
       // checks for valid date
       if (!dayjs(line.split(" ")[1], "DD-MM-YYYY", true).isValid()) {
@@ -37,131 +36,44 @@ fs.readFile(filename, "utf8", (err, data) => {
     }
 
     if (line.startsWith("ADD_SUBSCRIPTION")) {
-      // subsToAdd[line.split(" ")[1]] = line.split(" ")[2];
       let category = line.split(" ")[1],
-        renewalAmount = 0,
         type = line.split(" ")[2];
 
-      // console.log(category, type);
       switch (category) {
         case "MUSIC":
-          runner(category, type, startDate, 100, 250);
-          renewalAmount += helpers.renewalAmount;
-          // switch (type) {
-          //   case "FREE":
-          //     console.log(runner("MUSIC", startDate, 1, 10));
-          //     break;
-
-          //   case "PERSONAL":
-          //     renewalAmount += 100;
-          //     console.log(runner("MUSIC", startDate, 1, 10));
-          //   break;
-
-          //   case "PREMIUM":
-          //     renewalAmount += 250;
-          //     console.log(runner("MUSIC", startDate, 3, 10));
-          //     break;
-          // }
+          amount += runner(category, type, startDate, 100, 250);
           break;
 
         case "VIDEO":
-          runner(category, type, startDate, 200, 500);
-          renewalAmount += helpers.renewalAmount;
-          // switch (type) {
-          //   case "FREE":
-          //     console.log(runner("VIDEO", startDate, 1, 10));
-          //     break;
-
-          //   case "PERSONAL":
-          //     renewalAmount += 200;
-          //     console.log(runner("VIDEO", startDate, 1, 10));
-          //     break;
-
-          //   case "PREMIUM":
-          //     renewalAmount += 500;
-          //     console.log(runner("VIDEO", startDate, 3, 10));
-          //     break;
-          // }
+          amount += runner(category, type, startDate, 200, 500);
           break;
 
         case "PODCAST":
-          runner(category, type, startDate, 100, 300);
-          renewalAmount += helpers.renewalAmount;
-          // switch (type) {
-          //   case "FREE":
-          //     console.log(runner("PODCAST", startDate, 1, 10));
-          //     break;
+          amount += runner(category, type, startDate, 100, 300);
+          break;
+      }
+      subscriptions.push(category);
+    }
 
-          //   case "PERSONAL":
-          //     renewalAmount += 100;
-          //     console.log(runner("PODCAST", startDate, 1, 10));
-          //     break;
+    if (line.startsWith("ADD_TOPUP")) {
+      let devices = line.split(" ")[1],
+        months = line.split(" ")[2];
 
-          //   case "PREMIUM":
-          //     renewalAmount += 300;
-          //     console.log(runner("PODCAST", startDate, 3, 10));
-          //     break;
-          // }
+      switch (devices) {
+        case "FOUR_DEVICE":
+          amount += 50 * months;
+          break;
+
+        case "TEN_DEVICE":
+          amount += 100 * months;
           break;
       }
     }
-    if (line.startsWith("ADD_TOPUP")) {
-      topupToAdd[line.split(" ")[1]] = parseInt(line.split(" ")[2]);
-    }
   }
 
-  // for (const key in topupToAdd) {
-  //   switch (key) {
-  //     case "FOUR_DEVICE":
-  //       renewalAmount += 50 * topupToAdd[key];
-  //       break;
-
-  //     case "TEN_DEVICE":
-  //       renewalAmount += 100 * topupToAdd[key];
-  //       break;
-  //   }
-  // }
-
-  console.log("RENEWAL_AMOUNT " + `${renewalAmount}`);
+  console.log("RENEWAL_AMOUNT " + `${amount}`);
 });
 
-const runner = (
-  category,
-  type,
-  date,
-  // monthToAdd,
-  // daysToSubtract,
-  personalRenewalAmount,
-  premiumRenewalAmount
-) => {
-  switch (type) {
-    case "FREE":
-      printer(category, date, 1, 10);
-      break;
-
-    case "PERSONAL":
-      renewalAmount += personalRenewalAmount;
-      printer(category, date, 1, 10);
-      break;
-
-    case "PREMIUM":
-      renewalAmount += premiumRenewalAmount;
-      printer(category, date, 3, 10);
-      break;
-  }
-};
-
-function printer(category, date, monthToAdd, daysToSubtract) {
-  console.log(
-    "RENEWAL_REMINDER " +
-      category +
-      " " +
-      `${dayjs(date)
-        .add(monthToAdd, "month")
-        .subtract(daysToSubtract, "days")
-        .format("DD-MM-YYYY")}`
-  );
-}
 // // Subscriptions existance check //
 // if (!Object.keys(subsToAdd) || Object.keys(subsToAdd).length === 0) {
 //   console.log("SUBSCRIPTIONS_NOT_FOUND");
---
+++ helpers.js
@@ -0,0 +1,44 @@
+const dayjs = require("dayjs"),
+  customParseFormat = require("dayjs/plugin/customParseFormat"),
+  FREE_MONTHS_TO_ADD = 1,
+  PRESONAL_MONTHS_TO_ADD = 1,
+  PREMIUM_MONTHS_TO_ADD = 3,
+  DAYS_TO_SUBTRACT = 10;
+
+dayjs.extend(customParseFormat);
+
+module.exports = {
+  runner: (
+    category,
+    type,
+    date,
+    personalRenewalAmount,
+    premiumRenewalAmount
+  ) => {
+    switch (type) {
+      case "FREE":
+        printer(category, date, FREE_MONTHS_TO_ADD, DAYS_TO_SUBTRACT);
+        return 0;
+
+      case "PERSONAL":
+        printer(category, date, PRESONAL_MONTHS_TO_ADD, DAYS_TO_SUBTRACT);
+        return personalRenewalAmount;
+
+      case "PREMIUM":
+        printer(category, date, PREMIUM_MONTHS_TO_ADD, DAYS_TO_SUBTRACT);
+        return premiumRenewalAmount;
+    }
+  },
+};
+
+const printer = (category, date, monthToAdd, daysToSubtract) => {
+  console.log(
+    "RENEWAL_REMINDER " +
+      category +
+      " " +
+      `${dayjs(date)
+        .add(monthToAdd, "month")
+        .subtract(daysToSubtract, "days")
+        .format("DD-MM-YYYY")}`
+  );
+};
